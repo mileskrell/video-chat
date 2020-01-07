cd0f68d9fb01c9167ea50bf9bc51a817e496810d
Miles Krell <repos@mileskrell.com>
2020-01-07 13:39:36 -0500

    Turn SocketTestPage into a basic chat app

--- lib/socket_test_page.dart
+++ lib/socket_test_page.dart
@@ -3,23 +3,80 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:get_ip/get_ip.dart';
 
-class SocketTestPage extends StatefulWidget {
+class SocketChat extends StatefulWidget {
   @override
-  State<StatefulWidget> createState() => _SocketTestPageState();
+  State<StatefulWidget> createState() => _SocketChatState();
 }
 
-class _SocketTestPageState extends State<SocketTestPage> {
-  SocketState socketState = SocketState.STOPPED;
+class _SocketChatState extends State<SocketChat> {
   Socket socket;
   ServerSocket serverSocket;
 
   String ownIPAddress = "z"; // dummy value because it can't be "" or null
-  String otherIPAddress;
+  String otherIPAddress = "";
 
-  String sendText;
-  String messageText;
+  String sendText = "";
 
-  final controller = TextEditingController();
+  List<Message> messages = [];
+
+  final otherIPController = TextEditingController();
+  final sendMessageController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    startReceiving();
+  }
+
+  void startReceiving() async {
+    updateOwnIP();
+
+    socket?.destroy();
+    socket = null; // Because we check whether it's null or not later on.
+    serverSocket?.close();
+    final ss = await ServerSocket.bind(
+      InternetAddress.anyIPv4,
+      PORT,
+    );
+    ss.listen((socket) {
+      if (otherIPAddress == "") {
+        setState(() => otherIPAddress = socket.remoteAddress.address);
+        otherIPController.text = otherIPAddress;
+      }
+      socket.listen((data) {
+        setState(() {
+          messages.add(Message(String.fromCharCodes(data).trim(), false));
+        });
+      });
+    });
+    setState(() => this.serverSocket = ss);
+  }
+
+  Function createSender(BuildContext context) {
+    return () {
+      () async {
+        try {
+          if (socket == null ||
+              socket.remoteAddress.address != otherIPAddress) {
+            socket?.destroy();
+            socket = await Socket.connect(otherIPAddress, PORT);
+          }
+          socket.write(sendText + "\n");
+          setState(() => messages.add(Message(sendText, true)));
+          Scaffold.of(context).showSnackBar(
+            SnackBar(content: Text("Message sent!")),
+          );
+
+          sendMessageController.clear();
+          setState(() => sendText = "");
+        } on SocketException {
+          Scaffold.of(context).showSnackBar(
+            SnackBar(content: Text("ERROR: Couldn't send message")),
+          );
+        }
+      }();
+    };
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,114 +85,93 @@ class _SocketTestPageState extends State<SocketTestPage> {
         title: Text("Socket test"),
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
           ButtonBar(
             alignment: MainAxisAlignment.center,
             children: <Widget>[
               RaisedButton(
-                color: Colors.green,
-                child: Text("Send"),
-                onPressed: socketState == SocketState.SENDING
-                    ? null
-                    : () {
-                        serverSocket?.close();
-                        setState(() => socketState = SocketState.SENDING);
-                      },
-              ),
-              RaisedButton(
-                color: Colors.blue,
-                child: Text("Receive"),
-                onPressed: socketState == SocketState.RECEIVING
-                    ? null
-                    : () async {
-                        updateOwnIP();
-
-                        socket?.destroy();
-                        socket = null; // Because we check whether it's null or not later on.
-                        // TODO: Fix the UI so we're not doing that
-                        setState(() => socketState = SocketState.RECEIVING);
-                        serverSocket?.close();
-                        final ss = await ServerSocket.bind(
-                          InternetAddress.anyIPv4,
-                          PORT,
-                        );
-                        ss.listen((socket) {
-                          socket.listen((data) {
-                            setState(() {
-                              messageText = String.fromCharCodes(data).trim();
-                            });
-                          });
-                        });
-                        setState(() {
-                          this.serverSocket = ss;
-                        });
-                      },
-              ),
-              RaisedButton(
                 color: Colors.red,
-                child: Text("Stop"),
-                onPressed: socketState == SocketState.STOPPED
-                    ? null
-                    : () {
-                        serverSocket?.close();
-                        socket?.destroy();
-                        socket = null;
-                        setState(() => socketState = SocketState.STOPPED);
-                      },
+                child: Text("Restart"),
+                onPressed: () {
+                  serverSocket?.close();
+                  socket?.destroy();
+                  socket = null;
+                },
+              ),
+              IntrinsicWidth(
+                child: TextField(
+                  controller: otherIPController,
+                  decoration: InputDecoration(hintText: "IP address"),
+                  onChanged: (input) => setState(() => otherIPAddress = input),
+                ),
+              ),
+              IntrinsicWidth(
+                child: TextField(
+                  controller: sendMessageController,
+                  decoration: InputDecoration(hintText: "Message text"),
+                  onChanged: (input) => setState(() => sendText = input),
+                ),
+              ),
+              Builder(
+                builder: (context) => RaisedButton(
+                  child: Text("Send message"),
+                  onPressed: otherIPAddress != "" && sendText != ""
+                      ? createSender(context)
+                      : null,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(8),
+                child: ownIPAddress == "z"
+                    ? Text("Determining your IP address...")
+                    : RichText(
+                        text: TextSpan(children: [
+                        TextSpan(
+                            text: "Your IP address: ",
+                            style: TextStyle(color: Colors.black)),
+                        TextSpan(
+                          text: ownIPAddress,
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        )
+                      ])),
               ),
             ],
           ),
-          socketState == SocketState.SENDING
-              ? Column(
-                  children: <Widget>[
-                    IntrinsicWidth(
-                      child: TextField(
-                        decoration: InputDecoration(hintText: "IP address"),
-                        onChanged: (input) =>
-                            setState(() => otherIPAddress = input),
-                      ),
-                    ),
-                    IntrinsicWidth(
-                      child: TextField(
-                        controller: controller,
-                        decoration: InputDecoration(hintText: "Message text"),
-                        onChanged: (input) => setState(() => sendText = input),
-                      ),
-                    ),
-                    Builder(
-                      builder: (context) => RaisedButton(
-                        child: Text("Send message"),
-                        onPressed: () async {
-                          socket ??= await Socket.connect(otherIPAddress, PORT);
-                          socket.write(sendText + "\n");
-                          Scaffold.of(context).showSnackBar(
-                            SnackBar(content: Text("Message sent!")),
-                          );
-                          controller.clear();
-                          setState(() => sendText = null);
-                        },
-                      ),
-                    )
-                  ],
-                )
-              : socketState == SocketState.RECEIVING
-                  ? Column(
-                      children: <Widget>[
-                        Padding(
-                          padding: EdgeInsets.all(8),
-                          child: Text(
-                            ownIPAddress == "z"
-                                ? "Determining your IP address..."
-                                : "Your IP address: $ownIPAddress",
-                            style: TextStyle(fontWeight: FontWeight.bold),
+          if (messages.isEmpty)
+            Text(
+              "No messages yet!",
+              style: TextStyle(fontSize: 22),
+            ),
+          if (messages.isNotEmpty)
+            Flexible(
+              child: ListView(
+                  shrinkWrap: true,
+                  children: messages.map(
+                    (message) {
+                      return ListTile(
+                        title: Text(
+                          message.text,
+                          textAlign: message.ownMessage
+                              ? TextAlign.end
+                              : TextAlign.start,
+                          style: TextStyle(
+                            color:
+                                message.ownMessage ? Colors.blue : null,
                           ),
                         ),
-                        Text(messageText ?? "latest message will appear here"),
-                      ],
-                    )
-                  : Container(),
+                        leading:
+                            message.ownMessage ? null : Icon(Icons.tag_faces),
+                        trailing: message.ownMessage
+                            ? Icon(Icons.tag_faces, color: Colors.blue)
+                            : null,
+                      );
+                    },
+                  ).toList()),
+            ),
         ],
       ),
     );
@@ -149,10 +185,11 @@ class _SocketTestPageState extends State<SocketTestPage> {
   }
 }
 
-enum SocketState {
-  SENDING,
-  RECEIVING,
-  STOPPED,
-}
-
 const PORT = 5656;
+
+class Message {
+  final String text;
+  final bool ownMessage;
+
+  Message(this.text, this.ownMessage);
+}
